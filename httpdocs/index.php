d1e95020c48fcb875e3a1365cd9c9ec49e7593e1
Joan Teixidó <joan@laiogurtera.com>
2012-10-15 13:18:05 +0200

    Test placeholder for explorer 9

--- httpdocs/index.php
+++ httpdocs/index.php
@@ -10,85 +10,92 @@
 <body>
 <div class='container'>
 
-    <div id='top'>
-        <div class='row'>
-            <div class='span4 idiomes'>
-                <ul>
-                    <li id='ca'><a href=''>cat</a></li>
-                    <li id='en'><a href="">eng</a></li>
-                    <li id='es'><a href="">esp</a></li>
-                    <li id='fr'><a href="">fra</a></li>
-                </ul>
+    <div class='row' id='top'>
+        <div class='span4 idiomes'>
+            <ul>
+                <li id='ca'><a href=''>cat</a></li>
+                <li id='en'><a href="">eng</a></li>
+                <li id='es'><a href="">esp</a></li>
+                <li id='fr'><a href="">fra</a></li>
+            </ul>
+        </div>
+
+        <div class='span5 offset3 estacions'>
+            <p>Quin Cadaqués vols veure?</p>
+            <ul>
+                <li id='primavera'><a href="">PRIMAVERA</a></li>
+                <li id='estiu'><a href="">ESTIU</a></li>
+                <li id='tardor'><a href="">TARDOR</a></li>
+                <li id='hivern'><a href="">HIVERN</a></li>
+            </ul>
+
+
+        </div>
+    </div>
+
+    <div class='row'>
+
+        <div class='span12 head'>
+            <h1>CASA-DÚPLEX A CADAQUÉS | LLOGUER AMB ENCANT</h1>
+        </div>
+    </div>
+
+
+    <div id='center' class='row'>
+        <div class='span8' id='galeria'>
+            <div id='primary-image'>
+                <img class='contorn' src='/images/contorn-fotos.png'/>
+                <img class='primary-image' src='/images/cadaques-prova.png'/>
             </div>
+            <ul class='imatges'>
+                <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
+                <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
+            </ul>
+        </div>
 
-            <div class='span5 offset3 estacions'>
-                <p>Quin Cadaqués vols veure?</p>
-                <ul>
-                    <li id='primavera'><a href="">PRIMAVERA</a></li>
-                    <li id='estiu'><a href="">ESTIU</a></li>
-                    <li id='tardor'><a href="">TARDOR</a></li>
-                    <li id='hivern'><a href="">HIVERN</a></li>
-                </ul>
 
+        <div class='span4' id='right_column'>
+            <div id='calendar'>
 
+                <h2>Disponibilitat</h2>
+                <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=280&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=maristesbadalona.com_dkkv66gv3k180t0uil9a05l7b0%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=Europe%2FMadrid"
+                        style=" border-width:0 " width="250" height="280" frameborder="0" scrolling="no"></iframe>
+                <div id='booking'>
+                    <h2>Reserves</h2>
+
+                    <p class='highlight'>VOLS FER UNA RESERVA O UNA CONSULTA SOBRE ES PORTITXÓ; </p>
+                </div>
             </div>
         </div>
     </div>
-    <div class='span12 head'>
-        <h1>CASA-DÚPLEX A CADAQUÉS | LLOGUER AMB ENCANT</h1>
+
+
+    <div id='thumbs' class='row'>
+        <div class='span8'>
+            <ul id='menu-fotos'>
+                <li>Casa</li>
+                <li>Cuina</li>
+                <li>Menjador</li>
+                <li>Terrassa</li>
+                <li>Habitacions</li>
+                <li>Bayns</li>
+                <li>Planta</li>
+                <li>Cadaqués</li>
+            </ul>
+        </div>
     </div>
-    <div id='body'>
-        <div class='row'>
-            <div class='span8' id='galeria'>
-                <div id='primary-image'>
-                    <img class='contorn' src='/images/contorn-fotos.png'/>
-                    <img class='primary-image' src='/images/cadaques-prova.png'/>
-                </div>
-                <ul class='imatges'>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/cadaques-prova.png'/></a></li>
-                    <li><a href='#'><img width='50px' height="50px" src='/images/tardor.png'/></a></li>
-                </ul>
-            </div>
 
+    <div id='content' class='row'>
 
-            <div class='span4' id='right_column'>
-                <div id='calendar'>
-
-                    <h2>Disponibilitat</h2>
-                    <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=280&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=maristesbadalona.com_dkkv66gv3k180t0uil9a05l7b0%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=Europe%2FMadrid"
-                            style=" border-width:0 " width="250" height="280" frameborder="0" scrolling="no"></iframe>
-                    <div id='booking'>
-                        <h2>Reserves</h2>
-
-                        <p class='highlight'>VOLS FER UNA RESERVA O UNA CONSULTA SOBRE ES PORTITXÓ; </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class='row'>
-            <div class='span8'>
-                <ul id='menu-fotos'>
-                    <li>Casa</li>
-                    <li>Cuina</li>
-                    <li>Menjador</li>
-                    <li>Terrassa</li>
-                    <li>Habitacions</li>
-                    <li>Bayns</li>
-                    <li>Planta</li>
-                    <li>Cadaqués</li>
-                </ul>
-            </div>
-        </div>
-        <div class='row'>
-
-            <div class='span8 text'>
+        <div class='span8'>
+            <section class='description text'>
 
                 <div class='column'>
                     <div class='detail'>
@@ -125,19 +132,34 @@
                         como
                         por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
                 </div>
-            </div>
-
-            <div class='span4' id='menu'>
-                <span class='clip'></span>
-                <ul class='nav '>
-                    <li id='comarribar'><a href="">Com arribar-hi</a></li>
-                    <li id='tarifes'><a href="">Tarifes - 2012</a></li>
-                    <li id='condicions'><a href="">Condicions de contractació</a></li>
-                    <li id='serveis'><a href="">Serveis opcionals</a></li>
-                    <li id='mascotes'><a href="">Mascotes</a></li>
+            </section>
+            <section id='tarifes'>
+                <ul>
+                    <li>Juny 1a quinzena<span class='preu'>700 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
+                    <li>Juny 2a quinzena<span class='preu'>950 euros/setmana</span></li>
                 </ul>
+            </section>
+            <section id="condicions">
+                <p>blablabla</p>
+            </section>
+        </div>
 
-            </div>
+        <div class='span4 bs-docs-sidebar' id='menu'>
+            <span class='clip'></span>
+            <ul class='nav nav-list affix bs-docs-sidenav ' data-spy="affix">
+                <li id='comarribar_link'><a href="">Com arribar-hi</a></li>
+                <li id='tarifes_link'><a href="#tarifes">Tarifes - 2012</a></li>
+                <li id='condicions_link'><a href="#condicions">Condicions de contractació</a></li>
+                <li id='serveis_link'><a href="">Serveis opcionals</a></li>
+                <li id='mascotes_link'><a href="">Mascotes</a></li>
+            </ul>
         </div>
     </div>
 </div>
---
+++ httpdocs/test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta content="text/html; charset=UTF-8" http-equiv="content-type">
+    <title>Es portitxó</title>
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/styles.css" rel="stylesheet">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/placeholder.js"></script>
+<script type='text/javascript'>
+    $(function() {
+        $.setup_placeholders();
+    })
+</script>
+</head>
+<body>
+<div class='container'>
+    <h2>Provant si va el placehoder...</h2>
+    <form>
+        <input placeholder="Posa el teu nom... " type='text' />
+     </form>
+</div>
+
+</body>
+</html>
